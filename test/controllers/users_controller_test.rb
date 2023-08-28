3afe18ba37247887f783aa8a30c67453cc2d299e
shimizuarata <asone.runteq31@gmail.com>
2023-08-29 08:29:00 +0900

    Use SSL and the Puma web server in production

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "should get new" do
+  test "invalid signup information" do
     get signup_path
-    assert_response :success
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
   end
 end
